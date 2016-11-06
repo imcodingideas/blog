36fd0d6d7345261825ae3a9f4de719a19872fed8
Joseph Chambers <joseph@michael-chambers.com>
2016-11-06 12:13:34 -0700

    Setting up the app, model, and first route for the blogs

---
+++ app.js
@@ -0,0 +1,40 @@
+let express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+// APP CONFIG
+mongoose.connect('mongodb://localhost/blog_app');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// ROUTES CONFIG
+
+app.get('/', function (req, res) {
+   res.redirect('/blogs');
+});
+
+app.get('/blogs', function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('index', {blogs: blogs});
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Connect to the server');
+});
