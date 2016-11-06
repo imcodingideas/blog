f20d9ba2b949f8fb297ab0c28fcf4b7fc970dcc7
Joseph Chambers <joseph@michael-chambers.com>
2016-11-06 12:45:10 -0700

    created route for new and post

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.get('/', function (req, res) {
    res.redirect('/blogs');
 });
 
+
+// INDEX ROUTE
 app.get('/blogs', function (req, res) {
     Blog.find({}, function (err, blogs) {
         if(err) {
@@ -34,6 +36,26 @@ app.get('/blogs', function (req, res) {
         }
     });
 });
+
+// NEW ROUTE
+app.get('/blogs/new', function (req, res) {
+    res.render('new');
+});
+
+// CREATE ROUTE
+
+app.post('/blogs', function (req, res) {
+   //create blog
+    Blog.create(req.body.blog, function (err, newBlog) {
+       if(err){
+           res.render('new');
+       } else {
+           res.redirect('blogs');
+       }
+    });
+    //redirect
+});
+
 
 app.listen(3000, function () {
     console.log('Connect to the server');
