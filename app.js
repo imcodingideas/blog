745d8552d431007e9769a883a0b3b0d30e58b52c
Joseph Chambers <joseph@michael-chambers.com>
2016-11-06 21:18:19 -0700

    Added edit, update and delete restful routes and got it to work.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    methodOverride = require('method-override');
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost/blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -63,11 +65,47 @@ app.get('/blogs/:id', function (req, res) {
        if(err){
            res.redirect('/blogs');
        } else {
-           res.render('show', {blog: foundBlog})
+           res.render('show', {blog: foundBlog});
        }
     });
 });
 
+
+//EDIT
+app.get('/blogs/:id/edit', function (req, res) {
+   Blog.findById(req.params.id, function (err, foundBlog) {
+       if(err){
+           res.redirect('/blogs');
+       } else {
+           res.render('edit', {blog: foundBlog});
+       }
+   });
+});
+
+
+// UPDATE
+app.put('/blogs/:id', function (req, res) {
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+      if(err) {
+          res.redirect('/blogs');
+      } else {
+          res.redirect('/blogs/' + req.params.id);
+      }
+   });
+});
+
+
+//DELETE
+app.delete('/blogs/:id', function (req, res) {
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function (err) {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log('Connect to the server');
